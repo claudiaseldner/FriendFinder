f4ee2d33f72805319949ec155c95b78501c311ef
Claudia Seldner <claudiaseldner@Claudias-MacBook-Pro-2.local>
2019-07-17 21:44:35 -0500

    adding db to heroku

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 // DEPENDENCIES
 // Series of npm packages that we will use to give our server useful functionality
 // ==============================================================================
-
+var mysql = require("")
 var express = require("express");
 
 // ==============================================================================
