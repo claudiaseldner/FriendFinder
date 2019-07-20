68986efa7ff5214f31064d68819f88a9ab121676
Claudia Seldner <claudiaseldner@Claudias-MacBook-Pro-2.local>
2019-07-20 12:28:34 -0500

    adding jQuery Link

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -5,6 +5,7 @@
 // ===============================================================================
 
 var surveyArray = require("../data/friends");
+var waitListData = require("../data/friends")
 // var waitListData = require("../data/waitinglistData");
 
 
@@ -27,12 +28,6 @@ module.exports = function(app) {
     res.json(surveyArray);
   });
 
-  // API POST Requests
-  // Below code handles when a user submits a form and thus submits data to the server.
-  // In each of the below cases, when a user submits form data (a JSON object)
-  // ...the JSON is pushed to the appropriate JavaScript array
-  // (ex. User fills out a reservation request... this data is then sent to the server...
-  // Then the server saves the data to the tableData array)
   // ---------------------------------------------------------------------------
 
   app.post("/api/friends", function(req, res) {
@@ -47,17 +42,5 @@ module.exports = function(app) {
       waitListData.push(req.body);
       res.json(false);
     }
-  });
-
-  // ---------------------------------------------------------------------------
-  // I added this below code so you could clear out the table while working with the functionality.
-  // Don"t worry about it!
-
-  app.post("/api/clear", function(req, res) {
-    // Empty out the arrays of data
-    tableData.length = 0;
-    waitListData.length = 0;
-
-    res.json({ ok: true });
   });
 };
